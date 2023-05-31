b670b87f3104b6efb8ca9acc96a46abdceb1e718
Fakorede Abiola <abiolafakorede@gmail.com>
2023-05-31 16:34:40 +0100

    Broker service

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const PORT = "8086"
+
+type Config struct {
+}
+
+func main() {
+	app := Config{}
+
+	log.Printf("starting broke service on PORT %s\n", PORT)
+
+	srv := http.Server{
+		Addr:    fmt.Sprintf(":%s", PORT),
+		Handler: app.routes(),
+	}
+
+	// start server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
