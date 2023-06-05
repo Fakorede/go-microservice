b8601f7e6c48456358812ed0a5e2b0108e844d9c
Fakorede Abiola <abiolafakorede@gmail.com>
2023-06-05 23:49:58 +0100

    Connect mailer-service to the broker

--- README.md
+++ README.md
@@ -11,4 +11,10 @@ make start
 
 # Build and run other services
 make up_build
+
+# Stop frontend
+make stop
+
+# Stop all other services
+make stop
 ```
--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -11,6 +11,7 @@ type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
 	Log    LogPayload  `json:"log,omitempty"`
+	Mail   MailPayload `json:"mail,omitempty"`
 }
 
 type AuthPayload struct {
@@ -21,6 +22,13 @@ type AuthPayload struct {
 type LogPayload struct {
 	Name string `json:"name"`
 	Data string `json:"data"`
+}
+
+type MailPayload struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -46,9 +54,46 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 		app.authenticate(w, requestpayload.Auth)
 	case "log":
 		app.logAction(w, requestpayload.Log)
+	case "mail":
+		app.sendMail(w, requestpayload.Mail)
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
+}
+
+func (app *Config) sendMail(w http.ResponseWriter, msg MailPayload) {
+	jsonData, _ := json.MarshalIndent(msg, "", "\t")
+
+	mailServiceUrl := "http://mailer-service/send"
+
+	// call the auth service
+	request, err := http.NewRequest("POST", mailServiceUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling mail service"))
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Mail sent to: " + msg.To
+
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
 
 func (app *Config) logAction(w http.ResponseWriter, entry LogPayload) {
@@ -85,7 +130,6 @@ func (app *Config) logAction(w http.ResponseWriter, entry LogPayload) {
 	payload.Message = "Logged"
 
 	app.writeJSON(w, http.StatusAccepted, payload)
-
 }
 
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
